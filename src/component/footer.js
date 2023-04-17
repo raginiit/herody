ae87cea2a6925189366e7b619fe560de6c3e0471
rahiniit <kashyapraghu9526@gmail.com>
2023-04-17 23:38:29 +0530

    home page header footer

--- src/component/footer.js
+++ src/component/footer.js
@@ -1,6 +1,112 @@
-const Footer = () =>{
-    return <> Footer</>
-}
+import { Grid, Typography } from "@mui/material";
+import Image from "next/image";
+import PhoneIcon from "@mui/icons-material/Phone";
+import MailRoundedIcon from "@mui/icons-material/MailRounded";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
+const Footer = () => {
+  return (
+    <>
+      <Grid
+        container
+        direction="row"
+        justifyContent="start"
+        alignItems="center"
+        sx={{margin:4}}
+      >
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={4}
+          lg={6}
+          xl={6}
+          sx={{ height: 390, color: " #0b1460" }}
+        >
+          <Grid>
+            <h2>About Us</h2>
+            Herody helps brands to scale their business by breaking down their
+            complex<br></br>
+            <br></br>
+            business requirement into task and by taking end to end execution.
+            <br></br>
+            <br></br>
+            4th Floor,Classic converge,17th cross main Road,Sector 6,HSR Layout,
+            <br></br>
+            <br></br>
+          
+            Bengaluru,Karnatka 560102.
+            </Grid>
+          <Image src="/images/logo.png" height={80} width={80} alt="logo" />
+         
+          {/* 1 */}
+
+        </Grid>
+       
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={4}
+          lg={3}
+          xl={3}
+          sx={{ height: 390, color: "#0b1460" }}
+        >
+          <Grid >
+            <h2>Contact Us</h2>
+            <h3>Phone Number </h3>
+           
+            <PhoneIcon  />
+            <h3 style={{display:"inline", color:"blue" }}>02269124061</h3>
+
+            <h3 > Email Address</h3>
+            <MailRoundedIcon  />
+            <h3 style={{display:"inline",color:"blue"}}> help@herody.in</h3>
+           
+          </Grid>
+
+          {/* 2 */}
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={4}
+          lg={3}
+          xl={3}
+          sx={{ height: 390, color: "#0b1460" }}
+        >
+          <Grid >
+            <h2>Businesses</h2>
+            For any business related queries<br></br>
+            reach us out at sales@herody.in
+            <h2>
+              Follow us:
+              <FacebookIcon />
+              <InstagramIcon />
+              <LinkedInIcon />
+            </h2>
+          </Grid>
+          {/* 3 */}
+        </Grid>
+      </Grid>
+
+      <Grid
+        container
+        direction="column"
+        justifyContent="centre"
+        alignItems="center"
+      >
+        <Grid item xs={12} sx={{ color: "#0b1460", height: 80 }}>
+          <Grid item>
+            <h4>Copyright @2022 sHerody. All Right Reserved.</h4>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 
 export default Footer;
--- src/component/header.js
+++ src/component/header.js
@@ -1,7 +1,60 @@
-const Header = () =>{
-    return <>Headeghjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
-    r</>
-}
+import { Button, Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <>
+      <Box className="sticky" sx={{ backgroundColor: "#0b1460", color: "white", zIndex:1000 }}>
+        <Grid container justifyContent="center" alignItems="center">
+          {/* logo grid */}
+          <Grid item xs={3} sx={{ padding:1, paddingLeft:5}}>
+            <Image src="/images/herody.png" height={60} width={60} alt="logo" style={{backgroundColor:"white", borderRadius:4}}/>
+          </Grid>
+          <Grid item xs={7}>
+            {/* link grid */}
+            <Grid
+              container
+              direction="row"
+              justifyContent="space-evenly"
+              alignItems="center"
+            >
+              <Grid item>
+                <Typography className="headerButton"> Home</Typography>
+              </Grid>
+              <Grid item>
+                <Typography className="headerButton"> Activity</Typography>
+              </Grid>
+              <Grid item>
+                <Typography className="headerButton">Gigworkres</Typography>
+              </Grid>
+              <Grid item>
+                <Typography className="headerButton"> About</Typography>
+              </Grid>
+              <Grid item>
+                <Typography className="headerButton"> Contact</Typography>
+              </Grid>
+              <Grid item>
+                <Typography className="headerButton"> Register</Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          {/* for login grid */}
+          <Grid item xs={2}>
+            
+              <Box >
+                <Button color="secondary" variant="contained" sx={{backgroundColor:"white",color:"#0b1460"}}>Login</Button>
+                
+              </Box>
+           
+          </Grid>
+        </Grid>
+      </Box>
 
 
-export default Header
+    </>
+  );
+};
+
+export default Header;
